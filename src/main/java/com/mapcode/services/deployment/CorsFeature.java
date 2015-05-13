1d9bc73e1fb99dc2d17904d13db669da30c61314
Rijn Buve <rijn@buve.nl>
2015-05-13 21:47:34 +0200

    Commented CORS feature

--- src/main/java/com/mapcode/services/deployment/CorsFeature.java
+++ src/main/java/com/mapcode/services/deployment/CorsFeature.java
@@ -18,15 +18,32 @@ package com.mapcode.services.deployment;
 
 import org.jboss.resteasy.plugins.interceptors.CorsFilter;
 
+import javax.annotation.Nonnull;
 import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.FeatureContext;
 import javax.ws.rs.ext.Provider;
 
+/**
+ * This class adds the Cross-Origin Resource Sharing feature to allow Javascrip to call
+ * this service from another domain. It essentially add the calling domain to the list
+ * of allowed origins in the HTTP header.
+ *
+ * The feature is enabled by adding it to web.xml
+ *
+ * <pre>
+ *   &lt;context-param&gt;
+ *     &lt;param-name>resteasy.providers&lt;/param-name>
+ *     &lt;param-value>com.mapcode.services.deployment.CorsFeature&lt;/param-value>
+ *   &lt;/context-param>
+ * </pre>
+ */
 @Provider
 public class CorsFeature implements Feature {
 
     @Override
-    public boolean configure(final FeatureContext featureContext) {
+    public boolean configure(@Nonnull final FeatureContext featureContext) {
+
+        // Add the appropriate CORS header.
         final CorsFilter corsFilter = new CorsFilter();
         corsFilter.getAllowedOrigins().add("*");
         featureContext.register(corsFilter);
