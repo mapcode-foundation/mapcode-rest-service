d662114802446fccc9d1a53846425ff30c896b7f
Rijn Buve <rijn@buve.nl>
2017-07-28 15:57:59 +0200

    Updated doc

--- service/src/main/java/com/mapcode/services/dto/TerritoryDTO.java
+++ service/src/main/java/com/mapcode/services/dto/TerritoryDTO.java
@@ -99,7 +99,8 @@ public final class TerritoryDTO extends ApiDTO {
     @ApiModelProperty(
             name = "alphabets",
             value = "A list of typical alphabets (scripts) used in this territory, ordered by importance. " +
-                    "`ROMAN` is always present in this list, as a fallback.")
+                    "`ROMAN` is always present in this list, as a fallback.",
+            dataType = "com.mapcode.services.dto.AlphabetsDTO")
     @JsonProperty("alphabets")
     @JsonUnwrapped
     @XmlElementWrapper(name = "alphabets")
